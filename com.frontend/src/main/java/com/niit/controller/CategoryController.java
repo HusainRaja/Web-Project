10f4fcfcbb9bfd44bff05e45445ddf23ef734c20
Husain Raja <Husain Raja@DESKTOP-1RGUFPT>
2018-03-04 12:21:27 +0530

    added category functionalities

--- com.frontend/src/main/java/com/niit/controller/CategoryController.java
+++ com.frontend/src/main/java/com/niit/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,9 @@ public class CategoryController {
 	@Autowired
 	CategoryDAO categoryDAO;
 	@RequestMapping("/Category")
-	public String showCategory() {
+	public String showCategory(Model m) {
+		List<Category> list=categoryDAO.listCategories();
+		m.addAttribute("categoryList",list);
 		return "Category";
 	}
 	@RequestMapping(value="/InsertCategory",method=RequestMethod.POST)
@@ -28,6 +31,23 @@ public class CategoryController {
 		categoryDAO.addCategory(category);
 		List<Category> list=categoryDAO.listCategories();
 		m.addAttribute("categoryList",list);
-		return "Category";
+		return "redirect:/Category";
 	}
+	@RequestMapping(value="/deleteCategory/{cid}")
+	public String deleteCategory(@PathVariable("cid") int CID,Model m) {
+		System.out.println("category id ="+CID);
+		Category category=categoryDAO.getCategory(CID);
+		categoryDAO.deleteCategory(category);
+		List<Category> list=categoryDAO.listCategories();
+		m.addAttribute("categoryList",list);
+		return "redirect:/Category";
+		
+	}
+	@RequestMapping("editCategory/{cid}")
+	public String editCategory(@PathVariable("cid")int cId,Model m) {
+		Category category=categoryDAO.getCategory(cId);
+		m.addAttribute("categoryInfo",category);
+		return "redirect:/upateCategory";
+	}
+	
 }
